1807c403e343c8b2d7fb15c5e0f3fd4c029222e4
bminczu <bminczu@gmail.com>
2021-11-02 15:38:27 -0500

    first route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,17 @@
+import PostMessage from "../models/postMessage.js";
 
-export const getPosts = (req, res) => {
-    res.send("THIS WORKS");
+export const getPosts = async (req, res) => {
+    try{
+        const postMessage =  await PostMessage.find();
+        
+        res.status(200).json(postMessage);
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
+
 
 };
+
+export const createPost = (req, res) => {
+    res.send("Post Creation")
+}
