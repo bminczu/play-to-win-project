fcf46b6827f9798d87a3e46aa5c975a4debe8bf9
bminczu <bminczu@gmail.com>
2021-11-15 12:27:57 -0600

    fetching posts with redux

--- server/index.js
+++ server/index.js
@@ -11,12 +11,12 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 ////related to depreceated bodyparser https://stackoverflow.com/questions/69030963/error-usefindandmodify-is-an-invalid-option //
 app.use(cors());
+app.use('/posts', postRoutes)
 
 
 const CONNECTION_URL = "mongodb+srv://bart:password2@cluster0.pw4sd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
