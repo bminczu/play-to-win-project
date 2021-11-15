87500b41f196ebc2941a730ef516549478175db4
bminczu <bminczu@gmail.com>
2021-11-15 13:03:18 -0600

    post form WIP

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import useStyles from './styles';
+import FileBase from 'react-file-base64'
+import { TextField, Button, Typography, Paper  } from '@material-ui/core';
 
 const Form = () => {
+    const [postData, setPostData] = useState({creator: "", title: "", message: "", tags: "", selectedFile: ""})
     const classes = useStyles();
+
+    const handleSubmit = () => {
+
+    }
+
+
     return(
-        <h1>Form</h1>
+        <Paper className = {classes.paper}>
+            <form autoComplete="off" noValidate={classes.form} onSubmit={handleSubmit}>
+                <Typography variant="h6"> Creating Posts</Typography>
+                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onCHange={(e)=> setPostData({...postData, creator: e.target.value})}/>
+                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onCHange={(e)=> setPostData({...postData, title: e.target.value})}/>
+                <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onCHange={(e)=> setPostData({...postData, message: e.target.value})}/>
+                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onCHange={(e)=> setPostData({...postData, tags: e.target.value})}/>
+                <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({base64}) => setPostData({...postData, selectedFile: base64})} />
+                <Button className={classes.buttonSubmit} variant="contained" size="large" color="primary" type="submit" fullWidth>Submit</Button>
+                <Button variant="contained" color="secondary" size="small" onClick={clear} type="submit" fullWidth>Clear</Button>
+                </div>
+            </form>
+        </Paper>
     );
 }
 
