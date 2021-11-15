d41a99bd04260b816438aca816a0eae3d187996c
bminczu <bminczu@gmail.com>
2021-11-15 12:27:42 -0600

    fetching posts with redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
-import memories from './images/memories.png'
+import { useDispatch } from "react-redux";
+import memories from './images/memories.png';
+import {getPosts} from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import useStyles from './styles';
@@ -8,6 +10,13 @@ import useStyles from './styles';
 const App = ()=> {
 
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch])
+
+    
     return (
     <Container maxwidth="lg">
         <AppBar className={classes.appBar} position= "static" color="inherit">
@@ -16,7 +25,7 @@ const App = ()=> {
         </AppBar>
         <Grow in>
             <Container>
-                <Grid container justify="space-between" alignItems="stretch" spacing="3">
+                <Grid container justifyContent="space-between" alignItems="stretch" spacing='4'>
                     <Grid item xs={12} sm={7}>
                         <Posts/>
                     </Grid>
