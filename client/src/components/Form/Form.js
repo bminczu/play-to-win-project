a3441118dc59783d964268a847b870d8f80c5e05
bminczu <bminczu@gmail.com>
2021-11-16 15:43:15 -0600

    fixed routing bug

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,13 +1,21 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 import useStyles from './styles';
-import FileBase from 'react-file-base64'
+import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
 import { TextField, Button, Typography, Paper  } from '@material-ui/core';
+import { createPost } from '../../actions/posts';
 
 const Form = () => {
     const [postData, setPostData] = useState({creator: "", title: "", message: "", tags: "", selectedFile: ""})
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(createPost(postData))
+    }
+
+    const clear = () => {
 
     }
 
@@ -16,10 +24,10 @@ const Form = () => {
         <Paper className = {classes.paper}>
             <form autoComplete="off" noValidate={classes.form} onSubmit={handleSubmit}>
                 <Typography variant="h6"> Creating Posts</Typography>
-                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onCHange={(e)=> setPostData({...postData, creator: e.target.value})}/>
-                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onCHange={(e)=> setPostData({...postData, title: e.target.value})}/>
-                <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onCHange={(e)=> setPostData({...postData, message: e.target.value})}/>
-                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onCHange={(e)=> setPostData({...postData, tags: e.target.value})}/>
+                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e)=> setPostData({...postData, creator: e.target.value})}/>
+                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e)=> setPostData({...postData, title: e.target.value})}/>
+                <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e)=> setPostData({...postData, message: e.target.value})}/>
+                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e)=> setPostData({...postData, tags: e.target.value})}/>
                 <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({base64}) => setPostData({...postData, selectedFile: base64})} />
                 <Button className={classes.buttonSubmit} variant="contained" size="large" color="primary" type="submit" fullWidth>Submit</Button>
                 <Button variant="contained" color="secondary" size="small" onClick={clear} type="submit" fullWidth>Clear</Button>
--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ import postRoutes from './routes/posts.js';
 const app = express();
 
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 ////related to depreceated bodyparser https://stackoverflow.com/questions/69030963/error-usefindandmodify-is-an-invalid-option //
 app.use(cors());
 app.use('/posts', postRoutes)
@@ -25,4 +25,6 @@ const PORT = process.env.PORT|| 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
+
+
 
