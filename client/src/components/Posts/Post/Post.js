9e9542dfef306d5f4bc5912e3c16e72e07d157b2
bminczu <bminczu@gmail.com>
2021-11-23 14:40:14 -0600

    likes working

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,7 +5,7 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import DeleteIcon from '@material-ui/icons/Delete';
 import moment from 'moment';
 import {useDispatch} from 'react-redux';
-import {deletePost} from '../../../actions/posts'
+import {deletePost, likePost} from '../../../actions/posts'
 import useStyles from './styles';
 
 const Post = ({post, setCurrentId}) => {
@@ -38,7 +38,7 @@ const Post = ({post, setCurrentId}) => {
             <Typography  variant="h5" gutterBottom> {post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={()=> {}}>
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
 
                     <ThumbUpAltIcon fontSize="small"/>
                     Like
