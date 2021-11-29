f63fcfb78af90a37dc61e0ccbdff9e4ea70d7cc8
bminczu <bminczu@gmail.com>
2021-11-29 14:14:56 -0600

    Form edited

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -7,7 +7,7 @@ import { createPost, updatePost } from '../../actions/posts';
 
 
 const Form = ({currentId, setCurrentId}) => {
-    const [postData, setPostData] = useState({creator: '', title: '', message: '', tags: '', selectedFile: ''})
+    const [postData, setPostData] = useState({player: '', gameName: '', note: '', winner: '', selectedFile: ''})
     const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null );
     const classes = useStyles();
     const dispatch = useDispatch();
@@ -31,18 +31,18 @@ const Form = ({currentId, setCurrentId}) => {
 
     const clear = () => {
         setCurrentId(null);
-        setPostData({creator: '', title: '', message: '', tags: '', selectedFile: ''});
+        setPostData({player: '', gameName: '', note: '', winner: '', selectedFile: ''});
     }
 
 
     return(
         <Paper className = {classes.paper}>
             <form autoComplete="off" noValidate={classes.form} onSubmit={handleSubmit}>
-                <Typography variant="h6"> {currentId ? 'Editing' : 'Creating'} a Post </Typography>
-                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e)=> setPostData({...postData, creator: e.target.value})}/>
-                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e)=> setPostData({...postData, title: e.target.value})}/>
-                <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e)=> setPostData({...postData, message: e.target.value})}/>
-                <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e)=> setPostData({...postData, tags: e.target.value.split(',') })}/>
+                <Typography variant="h6"> {currentId ? 'Editing' : 'Logging'} a Game </Typography>
+                <TextField name="player" variant="outlined" label="Player" fullWidth value={postData.player} onChange={(e)=> setPostData({...postData, player: e.target.value})}/>
+                <TextField name="gameName" variant="outlined" label="Game Played" fullWidth value={postData.gameName} onChange={(e)=> setPostData({...postData, gameName: e.target.value})}/>
+                <TextField name="note" variant="outlined" label="Notes" fullWidth value={postData.note} onChange={(e)=> setPostData({...postData, note: e.target.value})}/>
+                <TextField name="winner" variant="outlined" label="winner (coma separated)" fullWidth value={postData.winner} onChange={(e)=> setPostData({...postData, winner: e.target.value.split(',') })}/>
                 <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({base64}) => setPostData({...postData, selectedFile: base64})} />
                 <Button className={classes.buttonSubmit} variant="contained" size="large" color="primary" type="submit" fullWidth>Submit</Button>
                 <Button variant="contained" color="secondary" size="small" onClick={clear} type="submit" fullWidth>Clear</Button>
--- client/src/styles.js
+++ client/src/styles.js
@@ -12,12 +12,12 @@ export default makeStyles((theme) => ({
   },
   heading: {
     color: 'black',
-    background: "#000000",
-  background: "-webkit-linear-gradient(to right, #000000 0%, #CF1512 78%)",
-  background: "-moz-linear-gradient(to right, #000000 0%, #CF1512 78%)",
-  background: "linear-gradient(to right, #000000 0%, #CF1512 78%)",
-  WebkitBackgroundClip: "text",
-  WebkitTextFillColor: "transparent",
+   
+
+
+background: "linear-gradient(to right, #000000 0%, #CF1512 78%)",
+WebkitBackgroundClip: "text",
+WebkitTextFillColor: "transparent",
   },
 
   image: {
