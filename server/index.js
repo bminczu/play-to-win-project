26c04ab7b5b3cb34dd109f77eb94924fd3f95a4e
bminczu <bminczu@gmail.com>
2021-11-30 20:16:56 -0600

    succesfully connected heroku

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
@@ -17,14 +17,19 @@ dotenv.config();
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
 app.use('/posts', postRoutes)
+
+app.get('/', (req, res) => {
+    res.send("Hello to the Play To Win API")
+});
 
 
 // const CONNECTION_URL = "mongodb+srv://bart:password2@cluster0.pw4sd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost: ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 
