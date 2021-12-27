81a0519645cae46e2b47013e2763f43c8ec6a882
bminczu <bminczu@gmail.com>
2021-12-27 11:40:29 -0600

    beggining on leaderboard table logic

--- client/src/components/TableComponent.js
+++ client/src/components/TableComponent.js
@@ -5,7 +5,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+
 
 import {useSelector} from 'react-redux'
 
@@ -18,11 +18,25 @@ const TableComponent = () => {
   const posts = useSelector(state => state.posts)
     
 
+
+///LEADERBOARD
+///iterate over each post object..
+// pull out the winning player key value pair
+//store in a seperate ariable
+/// count occurance of same player names
+/// return in descending order
+
+const topPlayers = (posts) => {
+  
+ return console.log(posts)
+}
+
+topPlayers(posts)
  
   return (
 
     
-      <TableContainer component={Paper}>
+      <TableContainer>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
