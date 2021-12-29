28b5731afcb7ff4ab669515eb1049c36f612e052
bminczu <bminczu@gmail.com>
2021-12-29 11:35:50 -0600

    NAvBar on login WIP

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,20 +1,36 @@
-import React from 'react'
-import {AppBar, Container, Typography} from '@material-ui/core'
-import useStyles from './styles'
+import React from 'react';
+import {AppBar, Avatar, Container, Typography} from '@material-ui/core';
+import {Link} from 'react-router-dom';
+import useStyles from './styles';
 import crown from '../../images/crown.png';
 
 
 const NavBar = () => {
 
     const classes = useStyles();
+    const user = null;
     return (
      
 
         <AppBar className={classes.appBar} position= "static" color="inherit">
-        <img className="heading-crown" src={crown} alt="crown" height="60"/>
-    <Typography className="heading" variant="h3"> PLAY TO WIN </Typography>
-        <img className="heading-crown" src={crown} alt="crown" height="60"/>
-</AppBar>
+            <div className={classes.brandContainer}> 
+
+            <img className="heading-crown" src={crown} alt="crown" height="60"/>
+            <Typography component={Link} to="/" className="heading" variant="h3"> PLAY TO WIN </Typography>
+            <img className="heading-crown" src={crown} alt="crown" height="60"/>
+            </div>
+            <Toolbar className={classes.toolbar}>
+            {user? (
+                <div className={classes.profile}>
+                    <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+                    <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                    <Button
+                </div>
+            ) : (
+
+            )}
+            </Toolbar>
+        </AppBar>
   
     )
 }
