0cb6f37a796e590399ba38a3ef9fb2512a6cd271
bminczu <bminczu@gmail.com>
2022-01-13 12:44:40 -0600

    new relic APM

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 
-   
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -8,7 +6,6 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
-
 
 const app = express();
 dotenv.config();
@@ -21,16 +18,17 @@ app.use(cors());
 app.use('/posts', postRoutes)
 
 app.get('/', (req, res) => {
-    res.send("Hello to the Play To Win API")
-});
+  res.send("Hello to the Play To Win API")
+}); 
 
 
 // const CONNECTION_URL = "mongodb+srv://bart:password2@cluster0.pw4sd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost: ${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+.then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost: ${PORT}`)))
+.catch((error) => console.log(`${error} did not connect`));
+
 
 
 
--- server/models/postData.js
+++ server/models/postData.js
@@ -1,3 +1,4 @@
+
 import mongoose from 'mongoose';
 
 const postSchema = mongoose.Schema({
