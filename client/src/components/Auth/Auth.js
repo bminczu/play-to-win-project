2475a766ccee4499af32339ce2798fc83cd0a452
bminczu <bminczu@gmail.com>
2022-01-18 15:58:02 -0600

    Built auth form + dynamic inputs

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,13 +1,40 @@
 import React from 'react'
+import { Avatar, Button, Paper, Grid, Container, Typography, TextField } from '@material-ui/core';
+import LockOutlinedIcon from 'material-ui/icons/LockOutlinedIcon';
+import Input from './input'
 
 const Auth = () => {
+    const classes = useStyles();
 
-    const state = null
-    
+    const isSignup = false;
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <div>
-            AUTH
-        </div>
+        <Container component="main" maxWidth="xs">
+            <Paper classes={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon/>
+                </Avatar>
+                <Typography variant="h5">{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {
+                        isSignup && (
+                            <>
+                                <Input name="firstName" label="First name" handleChnage={handleChange} autoFocus half/>
+                                <Input name="lastName" label="Last name" handleChnage={handleChange} half/>
+                            </>
+                         )}
+                         <Input name="email" label="Email address" handleChange={handleChange} type="email"/>
+                         <Input name="email" label="Email address" handleChange={handleChange} type="email"/>
+
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
     )
 }
 
